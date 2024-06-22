b61a009a1f01f9a9869a63c6d0912902233472aa
UoOsSe <143217955+ousey-ousey@users.noreply.github.com>
2024-06-22 19:56:42 +0200

    ?

--- src/index.js
+++ src/index.js
@@ -11,12 +11,12 @@ import { SpeedInsights } from "@vercel/speed-insights/react";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <SpeedInsights />
     <HelmetProvider>
-      <SpeedInsights />
       <ThemeProvider>
         <App />
       </ThemeProvider>
-      <SpeedInsights />
     </HelmetProvider>
+    <SpeedInsights />
   </React.StrictMode>
 );
