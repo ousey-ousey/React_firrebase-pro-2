6e49e6e489880bbfef0f29886e67b292de52d48c
UoOsSe <143217955+ousey-ousey@users.noreply.github.com>
2024-06-22 19:49:59 +0200

    insight

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { HelmetProvider } from "react-helmet-async";
 import "./i18n";
-
 // LEVEL2
 import { ThemeProvider } from "./context/ThemeContext";
 import App from "./App";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
